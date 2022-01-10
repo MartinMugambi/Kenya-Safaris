10102f3aab7693de2b47c7ccf77e3f358ed7f38c
Martin Mwenda <martinmwenda026@gmial.com>
2022-01-10 19:30:01 +0300

    header section

---
+++ client/src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import './header.css';
+const Header = () =>{
+    return(
+        <header className="header">
+          <div className="header__title">
+          <p>Kenya Safaris</p>
+           <img src="" alt="logo" />
+          </div>
+          <div className="header__button">
+           <button>Sigin</button>
+          </div>
+        </header>
+    );
+}
+
+export default Header;
