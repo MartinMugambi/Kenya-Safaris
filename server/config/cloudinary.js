5f8c8725261a7b89418d18d5aa6f1f7e748b57c1
Martin Mwenda <martinmwenda026@gmial.com>
2022-01-12 09:22:22 +0300

    production

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,8 +1,9 @@
 const cloudinary = require("cloudinary").v2;
+require('dotenv').config();
 cloudinary.config({ 
-    cloud_name: 'donwlbz6n', 
-    api_key: '361223377571554', 
-    api_secret: '6BlCihQbvk4sIIupIrV_nKWZZTc' 
+    cloud_name:  process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret 
   });
 
 module.exports = cloudinary;
